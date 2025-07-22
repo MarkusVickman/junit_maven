6821153efd703c82c6188a65c60ebbbbc1193128
Markus Vickman <markus.vickman@hotmail.com>
2025-07-22 10:31:30 +0200

    first

---
+++ demo/src/test/java/com/example/CalculatorTest.java
@@ -0,0 +1,28 @@
+package com.example;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
+public class CalculatorTest {
+
+    Calculator calc = new Calculator();
+
+    @Test
+    public void testAdd() {
+        assertEquals(5, calc.add(2, 3));
+    }
+
+    @Test
+    public void testDivide() {
+        assertEquals(2, calc.divide(10, 5));
+
+    }
+
+    @Test
+    public void testDivisionByZero() {
+        assertThrows(ArithmeticException.class, () -> calc.divide(10, 0));
+
+    }
+}
